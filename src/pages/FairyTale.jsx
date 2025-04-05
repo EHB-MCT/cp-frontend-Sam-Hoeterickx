a0d788b0591910534bd5c2f532c30dc9c7b55a48
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-04-05 17:58:18 +0200

    Test model

--- src/pages/FairyTale.jsx
+++ src/pages/FairyTale.jsx
@@ -1,9 +1,19 @@
 import { Canvas } from '@react-three/fiber'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+// const taxtiModel = () => {
+//     const gltf = useLoader(GLTFLoader, "models/taxti.gltf")
+// }
 
 const FairyTale = () => {
+    const gltf = useLoader(GLTFLoader, "models/taxti.gltf")
+
     return (
         <Canvas>
-           
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[10, 10, 5]} intensity={1} />
+            <primitive object={gltf.scene} scale={0.01} position={[0, 0, 0]} rotation={[0, Math.PI / 2, 0]} />
         </Canvas>
     )
 }
