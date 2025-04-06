193293fe5506276fb1ee3c2d57f5373fda6486b7
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-04-06 14:19:08 +0200

    Pigs in front of house

--- src/components/Scene3.jsx
+++ src/components/Scene3.jsx
@@ -11,29 +11,29 @@ const Scene3 = () => {
             <House
                 path={ "../../public/models/straw_house.glb" }
                 houseScale={ 1.5 }
-                housePosition={ [0, 0, 0] }
+                housePosition={ [0, 0, -1] }
                 houseRotation={ [0, 0, 0] }
                 pigScale={ 0.5 }
-                pigPosition={ [0, 0, 0] }
+                pigPosition={ [0, -1, 0.4] }
                 pigRotation={ [0, 0, 0] }
             />
             <House
-                path={ "../../public/models/straw_house.glb" }
+                path={ "../../public/models/wooden_house.glb" }
                 houseScale={ 1.5 }
-                housePosition={ [3, 0, 0] }
-                houseRotation={ [0, 0, 0] }
+                housePosition={ [-3.3, 0, 0] }
+                houseRotation={ [0, Math.PI * 0.25, 0] }
                 pigScale={ 0.5 }
-                pigPosition={ [3, 0, 0] }
-                pigRotation={ [0, 0, 0] }
+                pigPosition={ [-3, -1, 1.4] }
+                pigRotation={ [0, Math.PI * 0.25, 0] }
             />
             <House
-                path={ "../../public/models/straw_house.glb" }
+                path={ "../../public/models/stone_house.glb" }
                 houseScale={ 1.5 }
-                housePosition={ [-3, 0, 0] }
-                houseRotation={ [0, 0, 0] }
+                housePosition={ [3.3, 0, 0] }
+                houseRotation={ [0, - Math.PI * 0.25, 0] }
                 pigScale={ 0.5 }
-                pigPosition={ [-3, 0, 0] }
-                pigRotation={ [0, 0, 0] }
+                pigPosition={ [3, -1, 1.4] }
+                pigRotation={ [0, - Math.PI * 0.25, 0] }
             />
         </>
     )
