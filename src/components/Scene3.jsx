6d3b5f12d906b3dbd0bee2f88c21d66e82a9a09a
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-04-21 09:23:04 +0200

    Updated camera rotation

--- src/components/Scene3.jsx
+++ src/components/Scene3.jsx
@@ -10,8 +10,8 @@ import Tree from "./Tree.jsx";
 
 const Scene3 = () => {
     const center = [0, 0, -3];
-    const radius = 8;
-    const cameraHeight = 1.3;
+    const radius = 6;
+    const cameraHeight = 0.3;
 
     const numberOfHouses = 3;
     const angleIncrease = (2 * Math.PI) / numberOfHouses;
@@ -40,7 +40,7 @@ const Scene3 = () => {
 
     useEffect(() => {
         const trees = [];
-        for(let i = 0; i < 100; i++){
+        for(let i = 0; i < 150; i++){
             const angle = Math.random() * Math.PI * 2;
             const distance = 10 + Math.random()* 10;
             const x = Math.cos(angle) * distance;
