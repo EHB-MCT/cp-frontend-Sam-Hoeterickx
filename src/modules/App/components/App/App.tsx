5bcdca805544ea78484a9382fecc703ec3a933bb
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-04-24 21:00:39 +0200

    Added redirect to homepage if url is http://localhost:5173/

--- src/modules/App/components/App/App.tsx
+++ src/modules/App/components/App/App.tsx
@@ -1,6 +1,16 @@
-import { Outlet } from "react-router"
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router"
 
 export const App = () => {
+
+  const NAVIGATE = useNavigate();
+
+  useEffect(() => {
+    if(window.location.href === "http://localhost:5173/") {
+      NAVIGATE('/home')
+    }
+  }, [])
+
   return (
     <div>
       <h1>Hello world</h1>
