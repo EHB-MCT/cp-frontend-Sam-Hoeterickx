1204ff6b5dbd7750e2ffdaf601d5fc929db59d54
Sam-Hoeterickx <125275911+Sam-Hoeterickx@users.noreply.github.com>
2025-04-30 09:06:03 +0200

    Add comments to FairyTaleService for clarity on data fetching strategy

--- src/modules/shared/Services/fairy-tale-data/fairyTale.service.ts
+++ src/modules/shared/Services/fairy-tale-data/fairyTale.service.ts
@@ -10,6 +10,9 @@ class FairyTaleService {
 		});
     }
 
+    //CALL deze op een zo hoog mogelijk niveau en dan doorgeven? of kan ik met deze hook het beste het overall gaan callen 
+    //maar dan gaat er telkens een loading time zijn wat ook niet optimaal is en wat op het hoogste niveau maar 1 keer voorkomt
+
 }
 
 export const fairyTaleService = new FairyTaleService();
