774cc18babc5ed8987ff50e8ee99cc87e1a1b60e
Sam-Hoeterickx <125275911+Sam-Hoeterickx@users.noreply.github.com>
2025-04-30 10:12:46 +0200

    Same

--- src/modules/shared/Footer/Footer.tsx
+++ src/modules/shared/Footer/Footer.tsx
@@ -1,9 +1,11 @@
-import { Link, NavLink } from "react-router"
+import { NavLink } from "react-router"
 import clsx from "clsx"
 
 //Route
-import { ABOUT_US_ROUTE } from "../../App/components/AboutUs/about.route"
+import { ABOUT_US_ROUTE } from "../../App/components/AboutUs/about.route";
+import { ALL_FAIRY_TALES_ROUTE } from "../../App/components/AllFairyTales/allFairyTales.route";
 
+//Css
 import styles from './footer.module.scss';
 
 export const Footer = () => {
@@ -11,7 +13,9 @@ export const Footer = () => {
         <footer className={clsx(styles["footer"])}>
             <div className="outer-wrapper">
                 <div className={clsx(styles["footer--inner-wrapper"])}>
-                    <img className={clsx(styles["footer--inner-wrapper--logo"])} src="Logo_cp_front-end.svg" alt="Logo" />
+                    <NavLink to={ALL_FAIRY_TALES_ROUTE.path} >
+                        <img className={clsx(styles["footer--inner-wrapper--logo"])} src="Logo_cp_front-end.svg" alt="Logo" />
+                    </NavLink>
                     <div className={clsx(styles["footer--inner-wrapper--info"])}>
                         <p>23 studenten van de opleiding MCT die kozen ...</p>
                         <NavLink className="button" to={ABOUT_US_ROUTE.path}>About us</NavLink>
