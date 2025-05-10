7a973cd30a49f3b4814c302fca1a81ae8d03b72d
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-05-10 17:00:43 +0200

    Updated active state with build isActive from navlink

--- src/modules/shared/Navigation/Navigation.tsx
+++ src/modules/shared/Navigation/Navigation.tsx
@@ -1,46 +1,29 @@
-import { NavLink } from "react-router"
-import clsx from "clsx"
-import { useEffect, useState } from "react"
-
+import { NavLink } from "react-router";
+import clsx from "clsx";
 
 //Routes
-import { ABOUT_US_ROUTE } from "~app/components/AboutUs/about.route"
-import { ALL_FAIRY_TALES_ROUTE } from "~app/components/AllFairyTales/allFairyTales.route"
-import { EXPLORE_ROUTE } from "~app/components/Explore/explore.route"
+import { ABOUT_US_ROUTE } from "~app/components/AboutUs/about.route";
+import { ALL_FAIRY_TALES_ROUTE } from "~app/components/AllFairyTales/allFairyTales.route";
+import { EXPLORE_ROUTE } from "~app/components/Explore/explore.route";
 
 //Components
-import { SearchBar } from "~shared/SearchBar"
+import { SearchBar } from "~shared/SearchBar";
 
 //CSS
-import styles from "./navigation.module.scss"
+import styles from "./navigation.module.scss";
 
 export const Navigation = () => {
-    const [activeExploreState, setActiveExploreState] = useState<string>('');
-
-    let url = window.location.href;
-    const route = url.split('/');
-    const currentRoute = route[3];
-
-    useEffect(() => {
-        if(currentRoute === "explore"){
-            setActiveExploreState("active");
-        }
-    }, [route])    
-
-    return (
-        <div className="outer-wrapper">
-            <nav className={clsx(styles["navigation"])}>
-                <NavLink to={ALL_FAIRY_TALES_ROUTE.path}>
-                    <img className={clsx(styles['navigation--logo'])} src="/Logo_cp_front-end.svg" alt="Logo" />
-                </NavLink>
-                <div className={clsx(styles["navigation--links"])}>
-                    <NavLink className={clsx(styles[activeExploreState])} to={EXPLORE_ROUTE.path}>Explore</NavLink>
-                    <NavLink  to={ABOUT_US_ROUTE.path}>About</NavLink>
-                    <NavLink  to="/making-of/2">Making Of</NavLink>
-                    <SearchBar />
-                </div>
-            </nav>
+  return (
+    <div className="outer-wrapper">
+      <nav className={clsx(styles["navigation"])}>
+        <NavLink to={ALL_FAIRY_TALES_ROUTE.path}><img className={clsx(styles["navigation--logo"])} src="/Logo_cp_front-end.svg" alt="Logo" /></NavLink>
+        <div className={clsx(styles["navigation--links"])}>
+          <NavLink className={({ isActive }) => clsx({ [styles.active]: isActive })} to={EXPLORE_ROUTE.path}>Explore</NavLink>
+          <NavLink className={({ isActive }) => clsx({ [styles.active]: isActive })} to={ABOUT_US_ROUTE.path}>About</NavLink>
+          <NavLink className={({ isActive }) => clsx({ [styles.active]: isActive })} to="/making-of/2" > Making Of</NavLink>
+          <SearchBar />
         </div>
-        
-    )
-}
+      </nav>
+    </div>
+  );
+};
