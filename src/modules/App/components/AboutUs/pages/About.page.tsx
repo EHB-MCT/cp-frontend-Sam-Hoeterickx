aa1d83a4944c25a21dc950f7663161306d75799e
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-05-11 10:56:02 +0200

    CSS for About us images

--- src/modules/App/components/AboutUs/pages/About.page.tsx
+++ src/modules/App/components/AboutUs/pages/About.page.tsx
@@ -24,19 +24,21 @@ export const About = () =>  {
                     <h1>EHB STUDENTS</h1>
                     <p>
                         EHB-studenten Front-End Development werken dit semester met React aan een parallax website rond het thema sprookjes.
+                        <br />
                         Ze combineren techniek en creativiteit om een interactieve webervaring te creëren met diepte-effecten en animaties.
+                        <br />
                         Zo leren ze React-componenten, state management en visuele effecten toepassen om hun sprookjeswereld tot leven te brengen.
                     </p>
                 </div>
                 <div className={clsx(styles["about-wrapper--images-wrapper"])}>
-                    <img src="/about_image_1.jpg" alt="image of ehb students" />
-                    <img src="/about_image_2.jpg" alt="image of ehb students" />
-                    <img src="/about_image_3.jpg" alt="image of ehb students" />
-                    <img src="/about_image_4.jpg" alt="image of ehb students" />
-                    <img src="/about_image_1.jpg" alt="image of ehb students" />
-                    <img src="/about_image_2.jpg" alt="image of ehb students" />
-                    <img src="/about_image_3.jpg" alt="image of ehb students" />
-                    <img src="/about_image_4.jpg" alt="image of ehb students" />
+                    <img src="/about_image_1.jpg" alt="image of ehb students" className={clsx(styles["about-wrapper--images-wrapper--about_image_1"])} />
+                    <img src="/about_image_2.jpg" alt="image of ehb students" className={clsx(styles["about-wrapper--images-wrapper--about_image_2"])} />
+                    <img src="/about_image_3.jpg" alt="image of ehb students" className={clsx(styles["about-wrapper--images-wrapper--about_image_3"])} />
+                    <img src="/about_image_4.jpg" alt="image of ehb students" className={clsx(styles["about-wrapper--images-wrapper--about_image_4"])} />
+                    <img src="/about_image_1.jpg" alt="image of ehb students" className={clsx(styles["about-wrapper--images-wrapper--about_image_5"])} />
+                    <img src="/about_image_2.jpg" alt="image of ehb students" className={clsx(styles["about-wrapper--images-wrapper--about_image_6"])} />
+                    <img src="/about_image_3.jpg" alt="image of ehb students" className={clsx(styles["about-wrapper--images-wrapper--about_image_7"])} />
+                    <img src="/about_image_4.jpg" alt="image of ehb students" className={clsx(styles["about-wrapper--images-wrapper--about_image_8"])} />
 
                 </div>
             </div>
