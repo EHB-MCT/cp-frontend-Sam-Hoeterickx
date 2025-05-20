c0a5f77437cbb262b8823ae20e63f28f40c2bf94
Sam-Hoeterickx <125275911+Sam-Hoeterickx@users.noreply.github.com>
2025-05-20 21:45:02 +0000

    Deploying to gh-pages from @ EHB-MCT/cp-frontend-Sam-Hoeterickx@37552134e36c63994355bbe5317b810f50b4fc8a 🚀

---
+++ assets/HouseAndPig-B3Vf1Y8Z.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BluvZAAk.js";import{P as a,H as m}from"./House.model-Dpe0mclD.js";const j=({path:e,houseScale:i,housePosition:r,rotation:o,pigScale:t,pigPosition:n})=>s.jsxs(s.Fragment,{children:[s.jsx(a,{scale:t,position:n,rotation:o}),s.jsx(m,{path:e,scale:i,position:r,rotation:o})]});export{j as H};
