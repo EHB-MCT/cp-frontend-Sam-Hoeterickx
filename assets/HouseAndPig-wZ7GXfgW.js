fa3516092de3ac61e5d0b7464e1534c03cd6cb07
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-05-21 12:48:31 +0200

    Updates

---
+++ assets/HouseAndPig-wZ7GXfgW.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-4U1vD1DS.js";import{P as a,H as m}from"./House.model-DclpJRVz.js";const j=({path:e,houseScale:i,housePosition:r,rotation:o,pigScale:t,pigPosition:n})=>s.jsxs(s.Fragment,{children:[s.jsx(a,{scale:t,position:n,rotation:o}),s.jsx(m,{path:e,scale:i,position:r,rotation:o})]});export{j as H};
